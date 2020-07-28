0d48febb951805f3ec86253b0fe3e09766ad5834
Carlos Müller <kdu428@gmail.com>
2020-07-28 16:00:40 -0300

    [sql] fix primary key

--- src/main/resources/migrations/user.sql
+++ src/main/resources/migrations/user.sql
@@ -1,4 +1,4 @@
 create table user (
-    id bigint(20) not null AUTO_INCREMENT,
+    id bigint(20) not null AUTO_INCREMENT PRIMARY KEY,
     name varchar(255) default null
 ) engine=InnoDB charset=utf8 row_format=DYNAMIC;
